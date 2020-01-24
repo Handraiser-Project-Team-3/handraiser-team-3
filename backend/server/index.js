c33adb4c6410a15370d415d7c6f89eb0b1308904
Eric Atento <eric.atento@boom.camp>
2020-01-24 17:54:12 +0800

    initial server

---
+++ backend/server/index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const massive = require("massive");
+const cors = require("cors");
+
+const auth = require("./controllers/auth");
+
+massive({
+  host: "localhost",
+  port: 5432,
+  database: "handraiser",
+  user: "postgres",
+  password: "handraiserdb"
+}).then(db => {
+  const app = express();
+
+  app.set("db", db);
+  app.use(express.json());
+  app.use(cors());
+
+  //login & signup here
+
+  app.use(auth.headers);
+
+  //other pages that need headers
+
+  const port = 3001;
+
+  app.listen(port, () => {
+    console.log(`Server is Ready`);
+  });
+});
