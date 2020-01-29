ed0d4e907835226926eb5081a8698c8a269b0861
marthadansylemarbella <martha.marbella@boom.camp>
2020-01-29 10:46:20 +0800

    Done with Class Selection Page for Mentors, added modals and some features

--- frontend/src/components/routes/Routes.js
+++ frontend/src/components/routes/Routes.js
@@ -4,16 +4,16 @@ import { Login } from "../login/Login";
 import { Admin } from "../users/Admin";
 import { Mentors } from "../users/Mentors";
 import { Students } from "../users/Students";
-import Classroom from "../users/ClassSelection";
+import { Classroom } from "../users/mentors-class-view/MentorClassView";
 
 export const Routes = props => {
-  return (
-    <Switch>
-      <Route exact path="/" render={() => <Login data={props} />} />
-      <Route path="/admin" render={() => <Admin data={props} />} />
-      <Route path="/mentors" render={() => <Mentors data={props} />} />
-      <Route path="/student" render={() => <Students data={props} />} />
-      <Route path="/class" render={() => <Classroom data={props} />} />
-    </Switch>
-  );
+	return (
+		<Switch>
+			<Route exact path="/" render={() => <Login data={props} />} />
+			<Route path="/admin" render={() => <Admin data={props} />} />
+			<Route path="/mentors" render={() => <Mentors data={props} />} />
+			<Route path="/student" render={() => <Students data={props} />} />
+			<Route path="/class" render={() => <Classroom data={props} />} />
+		</Switch>
+	);
 };
