ce8d8e44e0717a4c8516ae1cf66fa78251db509e
josephazores <joseph.azores@gmail.com>
2020-01-29 11:22:12 +0800

    update dependencies

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,8 @@
 import "./App.css";
 import React, { useState } from "react";
 import { Routes } from "./components/routes/Routes";
-<<<<<<< HEAD
-import { HashRouter } from "react-router-dom";
-import { useLocalStorage } from "./components/hooks/useLocalStorage";
-=======
 import { BrowserRouter } from "react-router-dom";
 import useLocalStorage from "react-use-localstorage";
->>>>>>> 330ed027eebc84d0b11647e2f982eeef7a871084
 
 function App() {
 	const [metaData, setMetaData] = useState({});
